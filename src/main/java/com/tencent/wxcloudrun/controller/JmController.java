56e447e9e8fc5647f9a6c5ad503393a57c578fcd
yangpei <657842614@qq.com>
2024-05-01 14:51:59 +0800

    获取当前用户可抽奖次数

---
+++ src/main/java/com/tencent/wxcloudrun/controller/JmController.java
@@ -0,0 +1,20 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.config.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("jm")
+public class JmController {
+
+    final Logger logger = LoggerFactory.getLogger(CounterController.class);
+
+    @PostMapping("getValidCount")
+    public ApiResponse getValidCount(@RequestHeader("x-wx-openid")String openId) {
+        logger.info(openId);
+        return ApiResponse.ok();
+    }
+}
